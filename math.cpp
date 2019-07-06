d227c861a99f6f54ed22a65e4a8e2d7acdc5bfb2
shagunattri <shagunattri17@gmail.com>
2019-07-06 21:32:34 +0530

    cpp programs

---
+++ math.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+cout<<max(2,10)<<endl;
+cout<<min(29,2)<<endl;
+cout<<sqrt(169)<<endl;
+cout<<round(4.49)<<endl;
+cout<<log(3);
+
+return 0;
+}
---
+++ myfirstprogram.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+cout<<"YALO \n";
+std::cout<<"Hello"<<endl;
+std::cout<<"CrossMe"<<endl;
+
+int num = 15;
+const int numb = 69;
+cout<<num;
+cout<<"I am "<<numb<<"years old."<<endl;
+int sum = num + 2;
+cout<<"the sum is"<<sum<<endl;
+return 0;
+}
