0cb4c2398f153105ce8b982d9281d112bee5f40d
shagunattri <shagunattri17@gmail.com>
2021-01-21 23:48:49 +0530

    array ADT

---
+++ algo/ArrayADT/sortArray.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Array {
+    int A[10];
+    int size;
+    int length;
+};
+
+
+void Display(struct Array arr){
+    int i;
+    for(i=0;i<arr.length;i++)
+        printf("%d ",arr.A[i]);
+}
+
+int sort(struct Array arr){
+    for(int i =0;i<arr.length-1;i++){
+        if(arr.A[i]>arr.A[i+1])
+            return 0;
+    }
+    return 1;
+}
+
+int main(){
+    struct Array arr = {{2,3,9,16,18,21,28,32,35},10,9};
+    printf("%d \n",sort(arr));
+    Display(arr);
+    return 0;
+}
